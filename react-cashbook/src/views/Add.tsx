3b9c7d4dca1b24178eea52a8717ceb548ea2c7f3
leon <fumnan@163.com>
2022-08-01 15:24:22 +0800

    删除多余的TD标签

--- react-cashbook/src/views/Add.tsx
+++ react-cashbook/src/views/Add.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import type { RootState } from "../state/store";
 import { useSelector, useDispatch } from "react-redux";
